2e4e21043f74a7fd4960adbd88e392f40b519032
Robbie Mitchell <40429697@live.napier.ac.uk>
2021-04-14 12:10:34 +0100

    refactor queries 1-8

--- sqlqueries/6.sql
+++ sqlqueries/6.sql
@@ -1,7 +1,12 @@
---The top number of populated countries in a region where I choose the number.
-SELECT code, name, continent, region, population, capital
-FROM country
-WHERE region = 'Eastern Europe'
-ORDER BY population DESC
-LIMIT n 
-;
+--The top N populated countries in a region where N is provided by the user.
+SELECT 
+       code,
+       name,
+       continent,
+       region,
+       population,
+       capital
+FROM   country
+WHERE  region = 'Eastern Europe'
+ORDER  BY population DESC
+LIMIT  n ;
